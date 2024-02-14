65a91281f8f7dad13c19262af07d46381f4335f6
hallsopp <harryallsopp8@gmail.com>
2024-02-14 19:10:46 +0000

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //! # Ebiotic
 //!
-//! `ebiotic` provides a light-weight asynchronous interface for some popular Bioinformatics web services. It is designed to
+//! `ebiotic` provides a light-weight asynchronous interface for some popular bioinformatics web services. It is designed to
 //! enable access to the rich data and tools provided by institutes like the European Bioinformatics
-//! Institute's ([(EBI)](https://www.ebi.ac.uk/)) and the National Center for Biotechnology Information ([NCBI](https://www.ncbi.nlm.nih.gov/)). It's built to serialize
-//! and deserialze data using common formats like JSON and specialised bioinformatics formats like FASTA from
+//! Institute's ([EBI](https://www.ebi.ac.uk/)) and the National Center for Biotechnology Information ([NCBI](https://www.ncbi.nlm.nih.gov/)). It's built to serialize
+//! and deserialze data using common formats like JSON and specialised bioinformatics formats like FASTA using data structures from
 //! the [rust-bio](https://rust-bio.github.io/) library.
 //!
 //! ## Modules
@@ -20,18 +20,48 @@
 //! ebiotic = "0.0.1"
 //! ```
 //!
-//! ## Example
+//! ## Examples
+//!
+//! The idea of this crate is that all the services, regardless of the type, use a similar interface. This is achieved (currently) by using the `core::Service` trait and accompanying utilities.
+//! Therefore, this trait must be in scope to use any of the services. This can be done by importing it directly or by importing an entire module that contains it.
+//!
+//! Here's an example of how to use the `Blast` service:
 //!
 //! ```rust
 //! use ebiotic::tools::*;
 //!
 //! #[tokio::main]
 //! async fn main() {
-//!   let blast = Blast::default();
-//!   let query = "MAKQVQKARKLAEQAERYDDMAAAMKAVTEQGHELSNEERNLLSVAYKNVVGARRSSWRVISSIEQKTERNEKKQQMGKEYREKIEAELQDICNDVLELLDKYLIPNATQPESKVFYLKMKGDYFRYLSEVASGDNKQTTVSNSQQAYQEAFEISKKEMQPTHPIRLGLALNFSVFYYEILNSPDRACRLAKAAFDDASLAKDAESEKNPEEIAWYQSITQ";
-//!   let result = blast.run(query.to_string()).await;   
+//!    let blast = Blast::default();
+//!    let query = "MAKQVQKARKLAEQAERYDDMAAAMKAVTEQGHELSNEERNLLSVAYKNVVGARRSSWRVISSIEQKTERNEKKQQMGKEYREKIEAELQDICNDVLELLDKYLIPNATQPESKVFYLKMKGDYFRYLSEVASGDNKQTTVSNSQQAYQEAFEISKKEMQPTHPIRLGLALNFSVFYYEILNSPDRACRLAKAAFDDASLAKDAESEKNPEEIAWYQSITQ";
+//!    let result = blast.run(query.to_string()).await;
 //! }
 //! ```
+//!
+//! And this is an example of how to use the `Dbfetch` service:
+//!
+//! ```rust
+//! use ebiotic::data::*;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let dbfetch = Dbfetch::default();
+//!     let result = dbfetch.run("uniprot", "P12345").await;
+//! }
+//!```
+//!
+//! Despite being designed with asynchronicity in mind, the services can also be run synchronously by blocking on the thread. In the future I hope to provide an API for this.
+//!
+//! ```rust
+//! use ebiotic::tools::*;
+//! use tokio;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!    let blast = Blast::default();
+//!    let query = "MAKQVQKARKLAEQAERYDDMAAAMKAVTEQGHELSNEERNLLSVAYKNVVGARRSSWRVISSIEQKTERNEKKQQMGKEYREKIEAELQDICNDVLELLDKYLIPNATQPESKVFYLKMKGDYFRYLSEVASGDNKQTTVSNSQQAYQEAFEISKKEMQPTHPIRLGLALNFSVFYYEILNSPDRACRLAKAAFDDASLAKDAESEKNPEEIAWYQSITQ";
+//!    let result = tokio::task::block_in_place(|| blast.run(query.to_string()));
+//! }
 //!
 
 mod core;
